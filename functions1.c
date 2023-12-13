b329d222d4f2ae497e4f2e23605f6e5081cde55f
praisex64 <essiakpevwepraise@gmail.com>
2023-12-13 04:23:23 -0800

    removed trailing white space

---
+++ functions1.c
@@ -0,0 +1,27 @@
+#include "monty.h"
+
+/**
+ * stack_push - Function to handle the push opcode
+ * @stack: Double pointer to the top of the stack
+ * @line_num: Line number where the opcode appears in the file
+ *
+ * Return: void
+ */
+
+void stack_push(stack_t **stack, unsigned int line_num)
+{
+
+}
+
+/**
+ * stack_pall - Function to handle the pall opcode
+ * @stack: Double pointer to the top of the stack
+ * @line_num: Line number where the opcode appears in the file
+ *
+ * Return: void
+ */
+
+void stack_pall(stack_t **stack, unsigned int line_num)
+{
+
+}
