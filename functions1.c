e1d2735e491a677afcfb2735c127fa146594d456
praisex64 <essiakpevwepraise@gmail.com>
2023-12-13 06:11:00 -0800

    feature: push

--- functions1.c
+++ functions1.c
@@ -10,9 +10,46 @@
 
 void stack_push(stack_t **stack, unsigned int line_num)
 {
+	stack_t *new_node, *current_node = *stack;
+	int num_to_push;
 
+	if (!s.number)
+	{
+		free_global();
+		fprintf(stderr, "L%d: usage: push integer\n", line_num);
+		exit(EXIT_FAILURE);
+	}
+	num_checker(line_num);
+	num_to_push = atoi(s.number);
+
+	if (s.is_stack == 1)
+	{
+		new_node = malloc(sizeof(stack_t));
+		if (new_node == NULL)
+		{
+			free_global();
+			fprintf(stderr, "Error: malloc failed\n");
+			exit(EXIT_FAILURE);
+		}
+		new_node->n = num_to_push;
+		new_node->prev = NULL;
+		new_node->next = NULL;
+
+		if (!(*stack))
+			*stack = new_node;
+		else
+		{
+			while (current_node->next)
+			{
+				current_node = current_node->next;
+			}
+			new_node->prev = current_node; 
+			current_node->next = new_node;
+		}
+	}
+	else
+		queue_push(stack, num_to_push);
 }
-
 /**
  * stack_pall - Function to handle the pall opcode
  * @stack: Double pointer to the top of the stack
--- utility.c
+++ utility.c
@@ -1,4 +1,31 @@
 /**
+ * num_checker - checks if an array of chars is a number
+ * @line_num: line number
+ *
+ * Return: void
+ */
+
+void num_checker(unsigned int line_num)
+{
+	int curr_idx = 0;
+
+	if (s.number[curr_idx] == '-' || s.number[curr_idx] == '+')
+		curr_idx++;
+
+	while (s.number[curr_idx])
+	{
+		if (s.number[curr_idx] < '0' || s.number[curr_idx] > '9')
+		{
+			free_global();
+			fprintf(stderr, "L%d: usage: push integer\n", line_num);
+			exit(EXIT_FAILURE);
+		}
+
+		curr_idx++;
+	}
+}
+
+/**
  * free_stack - Frees the stack
  * @stack_head: head of the stack
  *
